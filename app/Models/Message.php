833ccb6387814b24748e88d7783ed7bcb30eed5f
Suhail Osman <35406282+sahlowle@users.noreply.github.com>
2024-06-04 02:59:09 +0300

    Update Message.php

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -21,7 +21,7 @@ class Message extends Model
      *
      * @return \Illuminate\Database\Eloquent\Casts\Attribute
      */
-    protected function firstName(): Attribute
+    protected function createdAt(): Attribute
     {
         return Attribute::make(
             get: fn ($value) => $this->created_at->diffForHumans(),

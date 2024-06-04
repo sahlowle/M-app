5115d44edd996f0b7e3cbcbc700dfaa805ee0f16
Suhail Osman <35406282+sahlowle@users.noreply.github.com>
2024-06-04 03:01:20 +0300

    Update Message.php

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -15,6 +15,7 @@ class Message extends Model
     protected $casts = [
         'created_at' => 'datetime:Y-m-d H:i:s',
         'updated_at' => 'datetime:Y-m-d H:i:s',
+        'is_seen' => 'boolean',
     ];
 
 

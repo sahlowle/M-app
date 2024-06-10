9d928172dd477f475ffff7b7dec2eee9c246bc9a
Suhail Osman <35406282+sahlowle@users.noreply.github.com>
2024-06-10 21:39:00 +0300

    Update Message.php

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -12,9 +12,11 @@ class Message extends Model
 
     protected $guarded = ['id'];
 
+    protected $dateFormat = 'Y-m-d H:i:s';
+
     protected $casts = [
-        'created_at' => 'datetime:Y-m-d H:i:s',
-        'updated_at' => 'datetime:Y-m-d H:i:s',
+        // 'created_at' => 'datetime:Y-m-d H:i:s',
+        // 'updated_at' => 'datetime:Y-m-d H:i:s',
         'is_seen' => 'boolean',
     ];
 
